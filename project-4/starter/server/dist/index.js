5fb5a155c6c787a6ea04839d19ce06510fcb89d8
anshuopinion <anshusopinion@gmail.com>
2023-05-12 08:47:10 +0530

    fixed file

--- project-4/starter/server/dist/index.js
+++ project-4/starter/server/dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
 app.use("/images", express_1.default.static(path_1.default.join(__dirname, "../public/images")));
 app.get("/", (req, res) => {
     console.log(path_1.default.join(__dirname, "../public"));
--- project-4/starter/server/src/index.ts
+++ project-4/starter/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import path from "path";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/images", express.static(path.join(__dirname, "../public/images")));
 
