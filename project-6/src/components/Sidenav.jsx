deb8542fa1c5efa49ac809acbcaca917c6903cd9
anshuopinion <anshusopinion@gmail.com>
2023-06-20 08:20:31 +0530

    code done

--- project-6/src/components/Sidenav.jsx
+++ project-6/src/components/Sidenav.jsx
@@ -2,7 +2,16 @@ import { Box, HStack, Heading, Icon, Stack, Text } from "@chakra-ui/react";
 import { RxDashboard } from "react-icons/rx";
 import { BsArrowDownUp } from "react-icons/bs";
 import { BiSupport } from "react-icons/bi";
+import { Link, useLocation } from "react-router-dom";
 const Sidenav = () => {
+  const location = useLocation();
+
+  console.log(location);
+
+  const isActiveLink = (link) => {
+    return location.pathname === link;
+  };
+
   const navLinks = [
     {
       icon: RxDashboard,
@@ -36,42 +45,48 @@ const Sidenav = () => {
         </Heading>
         <Box mt="6" mx="3">
           {navLinks.map((nav) => (
-            <HStack
-              borderRadius="10px"
-              key={nav.text}
-              py="3"
-              px="4"
-              _hover={{
-                bg: "#F3F3F7",
-                color: "#171717",
-              }}
-              color="#797E82"
-            >
-              <Icon as={nav.icon} />
-              <Text fontSize="14px" fontWeight="medium">
-                {nav.text}
-              </Text>
-            </HStack>
+            <Link to={nav.link} key={nav.text}>
+              <HStack
+                bg={isActiveLink(nav.link) ? "#F3F3F7" : "transparent"}
+                color={isActiveLink(nav.link) ? "#171717" : "#797E82"}
+                borderRadius="10px"
+                py="3"
+                px="4"
+                _hover={{
+                  bg: "#F3F3F7",
+                  color: "#171717",
+                }}
+                color="#797E82"
+              >
+                <Icon as={nav.icon} />
+                <Text fontSize="14px" fontWeight="medium">
+                  {nav.text}
+                </Text>
+              </HStack>
+            </Link>
           ))}
         </Box>
       </Box>
 
       <Box mt="6" mx="3" mb="6">
-        <HStack
-          borderRadius="10px"
-          py="3"
-          px="4"
-          _hover={{
-            bg: "#F3F3F7",
-            color: "#171717",
-          }}
-          color="#797E82"
-        >
-          <Icon as={BiSupport} />
-          <Text fontSize="14px" fontWeight="medium">
-            Support
-          </Text>
-        </HStack>
+        <Link to="/support">
+          <HStack
+            borderRadius="10px"
+            py="3"
+            px="4"
+            bg={isActiveLink("/support") ? "#F3F3F7" : "transparent"}
+            color={isActiveLink("/support") ? "#171717" : "#797E82"}
+            _hover={{
+              bg: "#F3F3F7",
+              color: "#171717",
+            }}
+          >
+            <Icon as={BiSupport} />
+            <Text fontSize="14px" fontWeight="medium">
+              Support
+            </Text>
+          </HStack>
+        </Link>
       </Box>
     </Stack>
   );
--- project-6/src/theme/index.js
+++ project-6/src/theme/index.js
@@ -110,6 +110,39 @@ export const theme = extendTheme(
           borderRadius: "10px",
         },
       },
+      FormLabel: {
+        baseStyle: {
+          fontSize: "sm",
+        },
+      },
+      Input: {
+        variants: {
+          outline: {
+            field: {
+              h: "38px",
+              borderRadius: "8px",
+              fontSize: "sm",
+              pb: "0",
+              _focus: {
+                boxShadow: "0 0 0 1px #5F00D9",
+              },
+            },
+          },
+        },
+      },
+      Textarea: {
+        variants: {
+          outline: {
+            h: "38px",
+            borderRadius: "8px",
+            fontSize: "sm",
+
+            _focus: {
+              boxShadow: "0 0 0 1px #5F00D9",
+            },
+          },
+        },
+      },
     },
   },
   withDefaultColorScheme({ colorScheme: "purple" })
